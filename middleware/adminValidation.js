01a42845d7e39f98cb319311164e591201676a9e
Bastian Bastias Sanchez <bastian.bastias@gmail.com>
2024-03-17 14:08:55 -0400

    added jwt as a cookie to the client

--- middleware/adminValidation.js
+++ middleware/adminValidation.js
@@ -3,13 +3,12 @@ const roleEnum = require("../models/roleEnum");
 require('dotenv').config();
 
 module.exports = (req, res, next) => {
-
   console.log('adminValidation middleware');
   const token = req.headers.authorization.split(' ')[1]; // Extract token after 'Bearer '
-  console.log('token', token);
+  //console.log('token', token);
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET); // Replace with your secret key
-    console.log('decoded::', decoded);
+    //console.log('decoded::', decoded);
     console.log('role::', decoded.profile);
     if (decoded.profile !== roleEnum.ADMIN) {
       return res.status(403).json({ message: 'Unauthorized access' });
@@ -18,6 +17,4 @@ module.exports = (req, res, next) => {
   } catch (err) {
     return res.status(403).json({ message: 'Invalid token' });
   }
-
-
 };
